485119164f396ba3b827d7117b058902d3c9f210
MauroFJ <zona.marques@gmail.com>
2026-06-04 02:24:19 -0300

    initial commit

---
+++ src/test/java/com/api/pratica/PraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
