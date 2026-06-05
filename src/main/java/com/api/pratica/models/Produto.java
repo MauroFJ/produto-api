88af2e4d65127822c34cacf4cc1e1d868445117f
MauroFJ <zona.marques@gmail.com>
2026-06-05 18:04:30 -0300

    feat: creating class Produto

--- src/main/java/com/api/pratica/models/Produto.java
+++ src/main/java/com/api/pratica/models/Produto.java
@@ -1,4 +1,70 @@
 package com.api.pratica.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "Coloque um nome!")
+    private String nome;
+
+    private int quantidade;
+    private double valor;
+    private String status;
+
+    public Produto() {}
+
+    public Produto(String nome, int quantidade, double valor, String status) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
