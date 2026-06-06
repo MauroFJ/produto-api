14536c3b68836279f6c12222c6b79a755b13d384
MauroFJ <zona.marques@gmail.com>
2026-06-06 00:02:03 -0300

    feat: ProdutoService

--- src/main/java/com/api/pratica/service/ProdutoService.java
+++ src/main/java/com/api/pratica/service/ProdutoService.java
@@ -1,4 +1,38 @@
 package com.api.pratica.service;
 
+import com.api.pratica.repository.ProdutoRepository;
+import com.api.pratica.models.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+    private final ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Produto> listAll() {
+        return repository.findAll();
+    }
+
+    public Produto save(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public Produto update(Produto produto, Long id) {
+        if(repository.existsById(id)) {
+            produto.setId(id);
+            return repository.save(produto);
+        }else {
+            throw new RuntimeException("Produto não encontrado");
+        }
+    }
+
+    public void delete(Long id) {
+            repository.deleteById(id);
+    }
 }
